a2003c45bf76d8750c5b32ca2693d1bbf7c492ec
joe.mercer118 <joe.mercer118@gmail.com>
2018-12-12 11:13:59 +0800

    intergraion probably done, but not sure

---
+++ integration.py
@@ -0,0 +1,17 @@
+def anonymous(x):
+    return x**2 + 1
+
+
+def integrate(fun, start, end):
+    step = 0.1
+    intercept = start
+    area = 0
+    while intercept < end:
+        intercept += step
+    # ''' your work here '''
+        area = anonymous(intercept)
+    # my job done
+    return area
+
+
+print(integrate(anonymous, 0, 10))
